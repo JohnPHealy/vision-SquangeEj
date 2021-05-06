ae5dca1d52bd1eb4c41f982b671283b2f807a8a8
Squange <78355315+SquangeEj@users.noreply.github.com>
2021-05-06 20:28:28 +0100

    Tutorial level has been completed

--- Vision game demo/Assets/Scripts/CameraDisable.cs
+++ Vision game demo/Assets/Scripts/CameraDisable.cs
@@ -4,13 +4,50 @@ using UnityEngine;
 
 public class CameraDisable : MonoBehaviour
 {
+    public GameObject intro;
     public GameObject PlayerCamera;
     // Start is called before the first frame update
+
+    private GameObject[] RedCubes;
+    private GameObject[] BlueCubes;
+    private int i =0;
     void Start()
     {
 
     }
-    [SerializeField]
+
+
+
+
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "intro")
+        {
+
+            i++;
+            if (i == 300)
+            {
+                FindObjectOfType<AudioManager>().Play("intro");
+                i = 0;
+                Destroy(col.gameObject);
+            }
+
+        }
+        if (col.gameObject.tag == "intro2")
+        {
+
+            i++;
+            if (i == 300)
+            {
+                FindObjectOfType<AudioManager>().Play("intro2");
+                i = 0;
+                Destroy(col.gameObject);
+            }
+
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -21,11 +58,28 @@ public class CameraDisable : MonoBehaviour
             PlayerCamera.SetActive(true);
 
 
+            RedCubes = GameObject.FindGameObjectsWithTag("Visible"); //get all RedCubes
+            foreach (GameObject Visible in RedCubes) //access them individually 
+            {
+                Visible.GetComponent<BoxCollider>().enabled = true;
+
+            }
+            BlueCubes = GameObject.FindGameObjectsWithTag("InVisible");
+            foreach (GameObject InVisible in BlueCubes) //access them individually 
+            {
+                InVisible.GetComponent<BoxCollider>().enabled = false;
+
+            }
+
+
 
         }
         //This is on entering in the player script, the exiting is on the cars script.
 
         // the CarComp is the script that gets enabled and along with the cameras attached to it. This is the script from above.
+
+       
+
 
         if (Input.GetKeyUp(KeyCode.E))
         {
@@ -33,6 +87,20 @@ public class CameraDisable : MonoBehaviour
             // gameObject.enabled = true;
 
             PlayerCamera.SetActive(false);
+
+
+            RedCubes = GameObject.FindGameObjectsWithTag("Visible"); //get all RedCubes
+            foreach (GameObject Visible in RedCubes) //access them individually 
+            {
+                Visible.GetComponent<BoxCollider>().enabled = false;
+
+            }
+            BlueCubes = GameObject.FindGameObjectsWithTag("InVisible");
+            foreach (GameObject InVisible in BlueCubes) //access them individually 
+            {
+                InVisible.GetComponent<BoxCollider>().enabled = true;
+
+            }
 
         }
     }
